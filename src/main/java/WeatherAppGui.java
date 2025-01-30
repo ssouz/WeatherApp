5f56ef30aa92ae08fdb7c5df44563afc7f043401
ssouz <souz.rs@outlook.com>
2025-01-30 02:47:26 -0300

    finalizar estrutura base do frame da GUI

---
+++ src/main/java/WeatherAppGui.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class WeatherAppGui extends JFrame {
+    public WeatherAppGui(){
+        super("WeatherApp");
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setSize(600, 800);
+
+        setLocationRelativeTo(null);
+
+        setLayout(null);
+
+        setResizable(true);
+
+
+    }
+}
